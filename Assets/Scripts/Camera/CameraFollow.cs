2976727f369cc0517266bfd55710771cfa6a0aaf
Victor <52900738+VihhdeoGame@users.noreply.github.com>
2020-10-02 16:41:21 -0300

    Program Character Movement/Create Base 3D Environment - SP1

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    [Range(0.01f, 10.0f)]
+    public float smoothFactor;
+    public Vector3 offset;  
+    void LateUpdate() 
+    {
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition =  Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.deltaTime);
+        transform.position = smoothPosition;
+        transform.LookAt(target);
+
+    }
+}
---
+++ Assets/Scripts/PC/SpriteRotation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class SpriteRotation : MonoBehaviour
+{
+    public Transform cam;
+
+    void Update()
+    {
+        Vector3 targetVector = cam.position - transform.position;
+        float newYAngle = Mathf.Atan2(targetVector.x,targetVector.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(45f,-1 * newYAngle,0f);       
+    }
+}
