9938afb4dab63cf737ba64d936728e5d9a6e32bc
Victor <52900738+VihhdeoGame@users.noreply.github.com>
2020-11-15 20:54:14 -0300

    Menu

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Animator animator;
+
+    public void OnStart()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        animator.SetBool("Start", true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(1);
+    } 
+
+    public void OnCredits()
+    {
+
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
+        Debug.Log("Has exited");
+    }
+}
